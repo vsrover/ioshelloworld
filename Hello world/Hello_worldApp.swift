3a5bb9573747bcfa7cdeee5a0632fdadd38c3a5e
vaha <sogomonyanva@vtb.ru>
2024-08-31 23:39:14 +0300

    Initial Commit

---
+++ Hello world/Hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_worldApp.swift
+//  Hello world
+//
+//  Created by vaha on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hello worldUITests/Hello_worldUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hello_worldUITestsLaunchTests.swift
+//  Hello worldUITests
+//
+//  Created by vaha on 31.08.2024.
+//
+
+import XCTest
+
+final class Hello_worldUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
